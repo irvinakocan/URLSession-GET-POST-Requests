0f040a96747e6312787dc52dc0b330ba3a696ae9
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-27 01:09:03 +0100

    Completed project

--- URLSession project/ViewController.swift
+++ URLSession project/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+let GET_ENDPOINT = "https://jsonplaceholder.typicode.com/users"
+let POST_ENDPOINT = "https://jsonplaceholder.typicode.com/posts"
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +17,95 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func getTapped(_ sender: Any) {
+        
+        // Create URL
+        guard let url = URL(string: GET_ENDPOINT) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                print("Error occured.")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Response Status code: \(response.statusCode)\n")
+            }
+            
+            // Converting json data into eye-readable data
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data)
+                    print(json)
+                }
+                catch {
+                    print("Was not able to convert data.")
+                    return
+                }
+            }
+        })
+        task.resume()
+    }
 
+    @IBAction func postTapped(_ sender: Any) {
+        
+        // Create URL
+        guard let url = URL(string: POST_ENDPOINT) else {
+            return
+        }
+        
+        // Create URL Request
+        var urlRequest = URLRequest(url: url)
+        
+        // Specify HTTP Method
+        urlRequest.httpMethod = "POST"
+        
+        // Set HTTP Request Header
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Specify parameters
+        let parameters = [
+            "username": "billy_who",
+            "tweet": "HelloWorld"
+        ]
+        
+        // Turning parameters into JSON
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters) else {
+            return
+        }
+        
+        // Setting HTTP Request httpBody (request message)
+        urlRequest.httpBody = httpBody
+        
+        let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                print("Error occured.")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Response Status code: \(response.statusCode)\n")
+            }
+            
+            // Converting json data into eye-readable data
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data)
+                    print(json)
+                }
+                catch {
+                    print("Was not able to convert data.")
+                    return
+                }
+            }
+        })
+        task.resume()
+    }
 }
 
